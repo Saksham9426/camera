0f3149c71a3f04aea4c897de0d0be1d16f62c14e
Saksham Aggarwal <134586621+Saksham9426@users.noreply.github.com>
2023-06-24 15:02:24 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,4 +1,5 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer as wc
 st.title('camera try')
-wc(key='key')
+wc(key='key', rtc_configuration={  # Add this config
+        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}])
