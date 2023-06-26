86954b0e2456ff46a6eb2cc15f437761ffb92731
Saksham Aggarwal <134586621+Saksham9426@users.noreply.github.com>
2023-06-26 15:21:10 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -7,7 +7,7 @@ from twilio.rest import Client
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = 'AC530c0ee59067abb9adf76f4cd45c6686'
-auth_token = 'ceda7665bac1676818311da2c5a5c418
+auth_token = 'ceda7665bac1676818311da2c5a5c418'
 client = Client(account_sid, auth_token)
 
 token = client.tokens.create()
