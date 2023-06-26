aff047d93cde1aeb08fe8c3efd772b8c52089c9a
Saksham Aggarwal <134586621+Saksham9426@users.noreply.github.com>
2023-06-26 16:59:37 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -7,8 +7,8 @@ import config
 
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
-account_sid = os.environ['TWILIO_ACCOUNT_SID']
-auth_token = os.environ['TWILIO_AUTH_TOKEN']
+account_sid = config.TWILIO_ACCOUNT_SID
+auth_token = config.TWILIO_AUTH_TOKEN
 client = Client(account_sid, auth_token)
 
 token = client.tokens.create()
