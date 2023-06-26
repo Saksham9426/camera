e049964020dbabfb93bc573771ba9453f6d1aec3
Saksham Aggarwal <134586621+Saksham9426@users.noreply.github.com>
2023-06-26 15:12:02 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -1,5 +1,20 @@
 import streamlit as st
 from streamlit_webrtc import webrtc_streamer as wc
+import os
+from twilio.rest import Client
+
+
+# Find your Account SID and Auth Token at twilio.com/console
+# and set the environment variables. See http://twil.io/secure
+account_sid = os.environ['AC530c0ee59067abb9adf76f4cd45c6686']
+auth_token = os.environ['ceda7665bac1676818311da2c5a5c418']
+client = Client(account_sid, auth_token)
+
+token = client.tokens.create()
+
+print(token.username)
+
 st.title('camera try')
-wc(key='key', rtc_configuration={  # Add this config
-        "iceServers": [{"urls": ["stun:stun.l.google.com:19302"]}]})
+wc(key='key',rtc_configuration={
+      "iceServers": token.ice_servers
+  }
