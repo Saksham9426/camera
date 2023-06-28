819b8a86b2fad4041d0d2e2dd523e09141871eee
Saksham Aggarwal <134586621+Saksham9426@users.noreply.github.com>
2023-06-28 12:50:23 +0530

    Update camera.py

--- camera.py
+++ camera.py
@@ -4,7 +4,7 @@ import os
 from twilio.rest import Client
 import config
 from streamlit_player import st_player
-
+import eel
 # Find your Account SID and Auth Token at twilio.com/console
 # and set the environment variables. See http://twil.io/secure
 account_sid = config.TWILIO_ACCOUNT_SID
@@ -19,4 +19,37 @@ st.title('camera try')
 wc(key='key',rtc_configuration={
       "iceServers": token.ice_servers
   })
-st_player('https://music.youtube.com/watch?v=sKK6MOdXrG0&list=OLAK5uy_k7Dmxf1eo5ViOyLf7aUJLXOlLj4DmpWS8')
+@eel.expose
+def song():
+      time.sleep(20)
+      return 1
+
+
+st.components.v1.html(
+      """<html> 
+<body> 
+
+<audio id="audio" autoplay>
+    <source src="https://drive.google.com/file/d/1H7yXAC1BNPAq7MEmdYLXrRkFq9vhOwiO/preview?usp=sharing">
+    
+</audio>
+<script> 
+let audio = document.getElementById("audio"); 
+let playlist = [
+    'https://drive.google.com/file/d/1jrV4cd9nZETOE9htso-xfFrpZEzLzkkh/preview?usp=sharing',
+    'https://drive.google.com/file/d/1cAqTnZ-gHXeJ5tOvY3Wai_ORr3uAZKln/view?usp=sharing',
+];
+async function getTime() {
+                let value = await eel.song()();
+                if (value == 1)
+                audio.src = playlist[1];
+                if (value == 2)
+                audio.src = playlist[0];
+                audio.play()
+            }
+</script> 
+
+</body> 
+</html>
+"""
+)
